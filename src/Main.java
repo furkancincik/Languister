3930431fc1703da9dcc51c1b090c1ba924fcf81c
Furkan Çinçik <furkancincik1999@hotmail.com>
2024-04-20 12:54:42 +0300

    update- 1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.xml.crypto.Data;
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/UserSettings.java
@@ -0,0 +1,2 @@
+public class UserSettings {
+}
