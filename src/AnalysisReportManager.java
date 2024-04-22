45d7da8dd1a1478cea1464acf1d475ec1cc4ebb6
Furkan Çinçik <furkancincik1999@hotmail.com>
2024-04-23 00:00:50 +0300

    update-1.6.7

--- src/AnalysisReportManager.java
+++ src/AnalysisReportManager.java
@@ -1,4 +1,7 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class AnalysisReportManager {
     private Connection connection;
@@ -7,5 +10,26 @@ public class AnalysisReportManager {
         this.connection = connection;
     }
 
-    
+    // 10 soru üzerinden kaç doğru yaptıgını döndürecek
+    public int getRecentCorrectAnswerCount(int userID) {
+        int correctAnswerCount = 0;
+        String sql = "SELECT COUNT(*) AS correct_count " +
+                "FROM (SELECT * FROM user_question_relationship " +
+                "      WHERE user_id = ? AND is_correct = true " +
+                "      ORDER BY answered_at DESC LIMIT 10) AS recent_answers;";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1,userID);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()){
+                correctAnswerCount=resultSet.getInt("correct_count");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return correctAnswerCount;
+
+    }
+
+
+
 }
